73721682e576a35daeb13c379acade77b244a0ae
promisingdev72 <promising.dev72@gmail.com>
2022-03-24 15:39:00 +0800

    update

---
+++ pages/homepage/index.js
@@ -0,0 +1,13 @@
+import Image from 'next/image'
+import Head from 'next/head'
+import Link from 'next/link'
+export default function HomePage() {
+    return(
+        <>
+            <Head>
+                <title>Home Page</title>
+            </Head>
+
+        </>
+    )
+}
