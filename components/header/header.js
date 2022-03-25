9ed8763aa2cca2380e5be4aae8f1bcf916cafad2
promisingdev72 <promising.dev72@gmail.com>
2022-03-25 08:50:39 +0800

    update

--- components/header/header.js
+++ components/header/header.js
@@ -1,9 +1,11 @@
-import styles from './header.module.css'
+import { FaFacebookF } from "react-icons/fa";
 
 export default function Header() {
     return (
-        <div>
-            
+        <div className='flex header'>
+            <div className = ''>
+                <div>dfdfd</div>
+            </div>
         </div>
     )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,23 @@
+import Image from 'next/image'
+import Head from 'next/head'
+import Link from 'next/link'
+import Header from '../components/header/header'
+import Footer from '../components/footer/footer'
+import Layout from '../components/layout/layout'
 
-export default function Home() {
+
+export default function HomePage() {
     return(
         <>
-        </>
+            <Layout>
+                <Head>
+                    <title>Home Page</title>
+                </Head>
+                <Header/>
+                
+                <Footer/>
+            </Layout>
+            
+        </>    
     )
 }
